f784442854ade5ebfa849a3dee88356803fae499
selenucar <slnucartx@gmail.com>
2021-09-08 21:17:51 -0500

    JS_Attribute

---
+++ index.js
@@ -0,0 +1,19 @@
+var x=10, y=20
+console.log(x+y)
+console.log('Ahmet')
+// alert('Be Careful!!!')  
+document.write('Ahmet')
+
+
+
+/*
+Create a website
+Website should have 
+Label : Number
+Input
+Button : CheckNumber
+Functionality: Page should check if a number  is even or odd
+Alert Message :  10 is even number
+Alert Message: 11 is odd number
+*/
+
